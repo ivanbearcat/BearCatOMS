e75dbbd37c5c2dd2a6c50f4aa6c12d1fd8d9e10c
BearCat <ivanhope@163.com>
2015-07-06 18:42:56 +0800

    fortress_server.py fixbug

--- fortress_server.py
+++ fortress_server.py
@@ -43,7 +43,7 @@ while 1:
         if hostname == '' or hostname not in all_servers:
             print '主机名不正确'
             continue
-        os.system('libs/audit_server/python audit_shell.py %s %s' % (server_lists[hostname],hostname))
+        os.system('python libs/audit_server/audit_shell.py %s %s' % (server_lists[hostname],hostname))
     except Exception:
         continue
     except KeyboardInterrupt:
